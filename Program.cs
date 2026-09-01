4945c6e4543191c97c753625cfc8aa9e493df8fe
Eamonn Watson <Eamonn.Watson@iqor.com>
2026-09-01 14:33:31 -0400

    Refactor Program.cs to use new service/endpoint extension methods and database initialization

--- Program.cs
+++ Program.cs
@@ -1,32 +1,21 @@
-using Microsoft.AspNetCore.Authentication.Negotiate;
+using Argo.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-//builder.Services.AddOpenApi();
-//builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
-//   .AddNegotiate();
-
-//builder.Services.AddAuthorization(options =>
-//{
-    // By default, all incoming requests will be authorized according to the default policy.
-//    options.FallbackPolicy = options.DefaultPolicy;
-//});
+builder.AddArgoServices();
 
 var app = builder.Build();
 
-app.UseDefaultFiles();   
+app.InitializeArgoDatabase();
+
+app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.MapOpenApi();
-//}
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
 
-
-
-
+app.MapArgoApi();
 
 app.Run();
